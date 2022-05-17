e93d05762004416ad4f785328ced3c395f849fae
Zakityy <39856414+Zakityy@users.noreply.github.com>
2022-05-17 00:41:21 -0700

    Delete java.js

--- java.js
+++
@@ -1,9 +0,0 @@
-function checker() {
-    var result = confirm('Are you sure?');
-    if (result == false) {
-        event.preventDefault();
-    }
-}
-
-console.log('hello if you are seeing this');
-
